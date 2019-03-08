1808ab8d3ec32e1df68b4ecf58423329efabb77a
Mickaël Allonneau <mickael.allonneau@gmail.com>
2019-03-08 15:29:04 +0100

    Added node/ (vanilla) example.

---
+++ node/index.js
@@ -0,0 +1,59 @@
+const readAuth = require('basic-auth');
+const serveStatic = require('serve-static');
+
+/*
+ *
+ */
+
+const auth = (req, res, next) => {
+  const credentials = readAuth(req);
+
+  // If request contains valid credentials
+  if (credentials && credentials.name == 'admin' && credentials.pass == 'admin') {
+    // proceed
+    next();
+  }
+  // Else, ask for authentication
+  else {
+    res.writeHead(401, { 'WWW-Authenticate': 'Basic realm="now-basic-auth-node"' });
+    res.end('Restricted area. Please login (admin:admin).');
+  }
+};
+
+const serve = serveStatic(__dirname + '/example');
+
+/*
+ *
+ */
+
+const app = (req, res) => {
+  // If requests admin area
+  if (req.url.startsWith('/admin')) {
+    // auth then serve
+    auth(req, res, () => {
+      serve(req, res, () => {
+        res.end('404 Not Found');
+      });
+    });
+  }
+  // Else (public area)
+  else {
+    // just serve
+    serve(req, res, () => {
+      res.end('404 Not Found');
+    });
+  }
+};
+
+/*
+ *
+ */
+
+module.exports = app;
+
+// Serve on localhost if asked to
+if (process.env.SERVE === 'true') {
+  const http = require('http');
+  const server = http.createServer(app);
+  server.listen(4444, () => console.log('Listening on port 4444...'));
+}
